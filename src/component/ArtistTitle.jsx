3cdb52f80331bc9de03c803fe2282d9cba94bfa5
Anubis1Good <vlad.polyakov.96@inbox.ru>
2022-06-22 12:28:51 +0300

    Problem with apply

--- src/component/ArtistTitle.jsx
+++ src/component/ArtistTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ArtistTitle(props) {
   return (
-    <div>
+    <div style={props.style}>
       <img src={props.urlImg} width={200} height={200}/>
       <h2>{props.title}</h2>
 
